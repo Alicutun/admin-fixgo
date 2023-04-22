311c018445a1be4f4ef8f40dcfa8c67957933151
Phat <xxtrantienphatxx@gmail.com>
2023-04-22 22:40:23 +0700

    done func ticketChecker

--- src/pages/TicketChecker.jsx
+++ src/pages/TicketChecker.jsx
@@ -1,34 +1,62 @@
-import { Box, Grid, Stack } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import "../components/topnav/topnav.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const TicketChecker = () => {
 	const [value, setValue] = useState("");
-	const [dataa, setData] = useState([]);
+	const [dataA, setData] = useState([]);
+	const [open, setOpen] = useState(false);
 	console.log("value", value);
-	console.log("data", dataa);
-	useEffect(() => {
-		const fetchData = async () => {
-			const { data } = await axios.get(
-				`https://backend-boo.vercel.app/api/checkBill/billToday/6380cf7343d0a2a65ce8a3a5/`
-			);
-			setData(data);
-		};
+	console.log("data", dataA);
+
+	const fetchData = async () => {
+		await setData([]);
+		const { data } = await axios.get(
+			`https://backend-boo.vercel.app/api/checkBill/billToday/${value}/`
+		);
+		await setData(data);
+	};
+	const handleSubmit = () => {
+		setOpen(true);
 		fetchData();
-	}, [value]);
+	};
+
 	return (
 		<div>
 			<Stack direction='row' justifyContent='center'>
 				<Box className='topnav__search' width='50%'>
 					<input
+						id='input'
 						type='text'
 						placeholder='Checking tickets...'
 						onChange={(e) => setValue(e.target.value)}
 					/>
-					<i className='bx bx-search'></i>
+					<i
+						className='bx bx-search'
+						style={{ cursor: "pointer" }}
+						onClick={handleSubmit}
+					></i>
 				</Box>
 			</Stack>
+
+			{open &&
+				(dataA.length > 0 ? (
+					<Stack
+						direction='column'
+						alignItems='center'
+						justifyContent='center'
+						marginTop='30px'
+						spacing={2}
+					>
+						<Typography>Data: {dataA[0].at}</Typography>
+						<Typography>Cinema: {dataA[0].cinema}</Typography>
+						<Typography>Movie: {dataA[0].movie}</Typography>
+						<Typography>Seat: {dataA[0].seat}</Typography>
+					</Stack>
+				) : (
+					<Typography> Not found </Typography>
+				))}
 		</div>
 	);
 };
