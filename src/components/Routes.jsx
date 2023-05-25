303b7d9aa6d27a5fd64da2ddc1151f1d0a681524
Phat <xxtrantienphatxx@gmail.com>
2023-05-25 07:27:05 +0700

    add UI add showtimes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
-import Users from "../pages/Users";
-import Orders from "../pages/Orders";
 import Movies from "../pages/Movies";
-import Discount from "../pages/Discount";
+import Orders from "../pages/Orders";
 import TicketChecker from "../pages/TicketChecker";
+import Users from "../pages/Users";
+import Showtimes from "../pages/Showtimes";
 
 const Routes = () => {
 	return (
@@ -14,7 +14,7 @@ const Routes = () => {
 			<Route path='/TicketChecker' component={TicketChecker} />
 			<Route path='/users' component={Users} />
 			<Route path='/movies' component={Movies} />
-			<Route path='/discount' component={Discount} />
+			<Route path='/showtimes' component={Showtimes} />
 			<Route path='/orders' component={Orders} />
 		</Switch>
 	);
---
+++ src/pages/Showtimes.jsx
@@ -0,0 +1,175 @@
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import React, { useState, useEffect } from "react";
+import {
+	Button,
+	FormControl,
+	Grid,
+	InputLabel,
+	MenuItem,
+	Select,
+	Stack,
+	TextField,
+} from "@mui/material";
+import axios from "axios";
+import dayjs from "dayjs";
+
+const Showtimes = () => {
+	const [movie, setMovie] = useState([]);
+	const [selectedMovie, setSelectedMovie] = useState("");
+
+	const [cinema, setCinema] = useState([]);
+	console.log("cinema:", cinema);
+	const [selectedCinema, setSelectedCinema] = useState("");
+
+	const [room, setRoom] = useState([]);
+	const [selectedRoom, setSelectedRoom] = useState("");
+
+	const [session, setSession] = useState([]);
+	const [selectedSession, setSelectedSession] = useState("");
+
+	const [showDate, setShowDate] = useState(dayjs(new Date()));
+
+	const fetchMovies = async () => {
+		const { data } = await axios.get(
+			`https://backend-boo.vercel.app/api/movies`
+		);
+		setMovie(data.reverse());
+	};
+
+	const fetchCinema = async () => {
+		const { data } = await axios.get(
+			`https://backend-boo.vercel.app/api/movies/cinemas`
+		);
+		setCinema(data.reverse());
+	};
+
+	const fetchRoom = async () => {
+		const { data } = await axios.get(
+			`https://backend-boo.vercel.app/api/movies/cinemaHalls`
+		);
+		setRoom(data.reverse());
+	};
+
+	useEffect(() => {
+		fetchMovies();
+		fetchCinema();
+		fetchRoom();
+	}, []);
+
+	const addShowtimes = async () => {
+		const data = {
+			idCinema: "6358b045169e41aaeeb68d6b",
+			time: "11:30",
+			idMovie: "6465a10e1cdc71e40d33569a",
+			price: 10,
+			image: "https://image.tmdb.org/t/p/w1280/qVYE8g6zNbTbaptUyWkCN7njkC3.jpg",
+			idHall: "636b51bee76256d36965d8bf",
+			startTime: "2023-05-26T00:00:00.000+00:00",
+			status: true,
+		};
+		await axios.post(
+			`https://backend-boo.vercel.app/api/movies/showing/add`,
+			data
+		);
+	};
+
+	return (
+		<div>
+			<h2 className='page-header'>Showtimes</h2>
+			<Grid container direction='column' spacing={2} padding='0 120px'>
+				<Grid item>
+					<FormControl fullWidth>
+						<InputLabel id='demo-simple-select-label'>Movie</InputLabel>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={selectedMovie}
+							label='Movie'
+							onChange={(event) => {
+								setSelectedMovie(event.target.value);
+							}}
+						>
+							{movie?.map((item) => (
+								<MenuItem value={item._id}>{item.name}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Grid>
+
+				<Grid item>
+					<FormControl fullWidth>
+						<InputLabel id='demo-simple-select-label'>Cinema</InputLabel>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={selectedCinema}
+							label='Cinema'
+							onChange={(event) => {
+								setSelectedCinema(event.target.value);
+							}}
+						>
+							{cinema?.map((item) => (
+								<MenuItem value={item._id}>{item.name}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Grid>
+				<Grid item>
+					<FormControl fullWidth>
+						<InputLabel id='demo-simple-select-label'>Room</InputLabel>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={selectedRoom}
+							label='Room'
+							onChange={(event) => {
+								setSelectedRoom(event.target.value);
+							}}
+						>
+							{room?.map((item) => (
+								<MenuItem value={item._id}>
+									{item.name} -&nbsp; <b>{item.totalSeats} seats </b>
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Grid>
+				<Grid item>
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DatePicker
+							label='Show Date'
+							value={showDate}
+							onChange={(newValue) => setShowDate(newValue)}
+						/>
+					</LocalizationProvider>
+				</Grid>
+				<Grid item>
+					<FormControl fullWidth>
+						<InputLabel id='demo-simple-select-label'>Session</InputLabel>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={selectedSession}
+							label='Session'
+							onChange={(event) => {
+								setSelectedSession(event.target.value);
+							}}
+						>
+							<MenuItem>9:00</MenuItem>
+							<MenuItem>10:30</MenuItem>
+							<MenuItem>1:00</MenuItem>
+							<MenuItem>2:30</MenuItem>
+						</Select>
+					</FormControl>
+				</Grid>
+
+				<Button sx={{ mt: "20px" }} onClick={addShowtimes}>
+					Add showtimes
+				</Button>
+			</Grid>
+		</div>
+	);
+};
+
+export default Showtimes;
